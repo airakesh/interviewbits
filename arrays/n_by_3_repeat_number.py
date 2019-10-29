7b944e1da1a4d315639a51b836d478f168826102
Rakesh Kumar <40352675+airakesh@users.noreply.github.com>
2019-10-29 13:39:06 +0530

    N/3 Repeat Number

--- arrays/n_by_3_repeat_number.py
+++ arrays/n_by_3_repeat_number.py
@@ -1,6 +1,6 @@
 '''
-You’re given a read only array of n integers. Find out if any integer occurs 
-more than n/3 times in the array in linear time and constant additional space.
+You’re given a read only array of n integers. Find out if any integer occurs more than 
+n/3 times in the array in linear time and constant additional space.
 
 If so, return the integer. If not, return -1.
 

4f66145f6b7d6b2c6aa9c585392de246f1441018
Rakesh Kumar <40352675+airakesh@users.noreply.github.com>
2019-11-07 14:02:28 +0530

    Length of Last Word

---
+++ strings/length_of_last_word.py
@@ -0,0 +1,30 @@
+'''
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+Example:
+
+Given s = "Hello World",
+
+return 5 as length("World") = 5.
+
+Please make sure you try to solve this problem without using library functions. Make sure you only traverse the string once.
+'''
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def lengthOfLastWord(self, A):
+        words = A.split()
+        
+        return len(words[-1]) if len(words) > 0 else 0
+        
+        
+# Use CASES
+if __name__ == '__main__':
+    s = Solution()
+    A = 'Hello World'
+    print(s.lengthOfLastWord(A))
